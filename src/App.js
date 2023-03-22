b97fa54d1abece6745e6e02dcc8af4a105d2cfcc
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-03-22 14:49:16 +0000

    Basic Home page layout and theme variable created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { purple } from '@mui/material/colors';
+
+// overriding material ui's default themes 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#454B1B',
+    },
+    secondary: purple // purple is built in. All secondary colors (main, light and dark) will be this color
+  },
+  typography: {
+    fontFamily: 'Delicious Handrawn',
+    fontWeight: 400
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Home />
+    </ThemeProvider>
   );
 }
 
