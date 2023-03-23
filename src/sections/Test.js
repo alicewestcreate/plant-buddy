394501fb4fd5608266acd8dc16fe834bd8619f12
Alice <alicewest.create@gmail.com>
2023-03-23 02:24:55 +0000

    match issue

--- src/sections/Test.js
+++ src/sections/Test.js
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material";
+import { all } from "q";
 import React, { useState, useEffect } from "react";
-import {fetchData} from "../utils/API";
+import { fetchData } from "../utils/API";
 
 const Test = () => {
   const [data, getData] = useState([]);
@@ -19,76 +20,83 @@ const Test = () => {
       .then((data) => getData(data));
   };
 
-//   console.log(data);
-
-  // Variables that simulate the questions. 
-  let property = "Appeal"
-  let value = "Robustness"
+  //   console.log(data);
 
 
-  // Filter results, pass through the data, the property that is being targeted and the value we're filtering against. 
-  const filterResults = (data, property, value) => {
-    function filterByProperty(plant) {
 
-        // If the plant property is equal to value in question, return ture. 
+
+  //////////////////////////////////////////////////////////////////////
+// Filter results, pass through the data, the property that is being targeted and the value we're filtering against.
+    const filterResults = (data, property, value) => {
+        console.log(property);
+      function filterByProperty(plant) {
+        // If the plant property is equal to value in question, return ture.
         if (plant[property] === value) {
-            console.log(plant);
-            return true
+        //   console.log(plant);
+          return true;
         } else {
-            return false
+          return false;
         }
-    }
+      }
+      // All true returns are stored into this variable.
+      let filteredResultsArray = data.filter(filterByProperty);
 
-    // All true returns are stored into this variable. 
-    let filteredResultsArray = data.filter(filterByProperty);
-
-    // For each true return, add a new properity called priority points, set the value to one. 
-    filteredResultsArray.forEach((result) => {
-        result["Priority Points"] = 1 
-    })
-    return filteredResultsArray
-  }
-
-
-// Call the function to get filtered results. 
-let filteredResultsArray = filterResults(data, property, value)
+      // For each true return, add a new properity called priority points, set the value to one.
+      filteredResultsArray.forEach((result) => {
+        result["Priority Points"] = 1;
+      });
+      console.log(property, filteredResultsArray);
+      return filteredResultsArray;
+    };
 
 
+//////////////////////////////////////////////////////////////////////
+// Call the function to get filtered results.
 
- console.log(filteredResultsArray);
+const compareResults = (allFilteredResults, filteredResultsArray) => {
+    console.log("filteredd",filteredResultsArray);
+    console.log("all results",allFilteredResults);
+
+      const matches = allFilteredResults.filter((item) => {
+        console.log("item",item);
+        return filteredResultsArray.includes(item)
+      });
+      console.log("matches",matches);
+      matches.forEach(match => {
+        console.log("MARCH", match);
+        
+      });
+
+
+
+    };
+
+
+// Variables that simulate the questions.
+    let questions = ["Do you kill plants", "Do you water plants"]
+    let allFilteredResults = null;
+  
+questions.forEach((question, index) => {
+    console.log(question);
+    let property = ["Appeal", "Avaibility"];
+    let value = ["Robustness", "Seasonal" ];
+    let filteredResultsReturned = filterResults(data, property[index], value[index]);
+
+    if (allFilteredResults === null) {
+        allFilteredResults = [];
+        allFilteredResults.push(filteredResultsReturned);
+      } else {
+        compareResults(allFilteredResults, filteredResultsReturned);
+      }
+
+
+  });
   
 
 
-
-
-
-//   let allFilteredItems = []
-//   const getFilterItems = (data) => {
-
-//             // function to check even numbers
-//             function filterByProperty(property) {
-//               // console.log("PROP",property.Appeal);
-//                 if (property.Appeal === "Robustness" )
-//                 return true;
-//               // else
-//               //   return false;
-//             }
   
-//
-//       let filteredResults = data.filter(filterByProperty);
-//       // console.log("filtered",filteredResults);
-
-//       filteredResults.forEach(result => {
-//         result["PriorityPoints"] = 1
-//         // console.log(result);
-//       });
-//       setFilter(filteredResults)
-//       // allFilteredItems.push(filter)
-//       // console.log("ALL", allFilteredItems);
-//       return filter
-
-//   }
-
+  
+ 
 
 
   return (
@@ -100,48 +108,29 @@ let filteredResultsArray = filterResults(data, property, value)
 
 export default Test;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Start Quiz
 
-// 1. Fetch API Data  
-    // Return a variable with all result inside. 
+// 1. Fetch API Data
+// Return a variable with all result inside.
 
-
-// 2. Ask a question about a property 
-    // Return the value of the property
-
+// 2. Ask a question about a property
+// Return the value of the property
 
 // 3.  Filter Function(data + property)
-  // Pass through the data, and filter out plants that have that property. 
+// Pass through the data, and filter out plants that have that property.
 
 // 4. Add new key "priorityPoint" with value of 1
-    // Return filteredResults
-
-
+// Return filteredResults
 
 // 5.  If (allFilteredRsults EQUAL Null)
-    // Push results to allFilteredResults 
+// Push results to allFilteredResults
 // 6/ Go to function >> Compare Results (results & allFilteredResults )
-        // if a result === a filteredResult 
-            // Add 1 Priority Point to filteredResult.PriorityPoint
-            // return (without adding it to the allFiltered Results)
-        // else push result to allFiltedResults 
+// if a result === a filteredResult
+// Add 1 Priority Point to filteredResult.PriorityPoint
+// return (without adding it to the allFiltered Results)
+// else push result to allFiltedResults
 
-
-
-// Object: 
+// Object:
 
 // Question: "Are you a plant whisper or plant killer"
 // Choices: [[true, Whisper], [false, Killer] ]
