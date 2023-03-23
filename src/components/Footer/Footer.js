77dd3656c58f61ba0733afefbf651c57d5b0ecc0
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-23 21:29:23 +0000

    Add function for responsive app bar for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,3 +4,30 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+
+
+function ResponsiveAppBar() {
+    return (
+      <>
+        <Box sx={{ paddingBottom: '64px' }}></Box>
+        <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </>
+    );
+  }
+  export default ResponsiveAppBar;
+  
