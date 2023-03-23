75f39c02b4bd86b299e26a2af32f806594ee0ca3
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-23 21:24:58 +0000

    Ad first import codes

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
