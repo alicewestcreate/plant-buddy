2c4f0b249f53cecd0a08a8c12c5b1918875510d3
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-24 13:39:55 +0000

    Add comments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,15 +6,23 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
 
+// Define an array of page names
 const pages = ['About', 'Contact'];
 
 function ResponsiveAppBar() {
 return (
     <>
+    {/* Add padding to the bottom of the page */}
     <Box sx={{ paddingBottom: '64px' }}></Box>
+
+    {/* AppBar component with fixed positioning at the bottom of the page */}
     <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
         <Container maxWidth="xl">
+
+            {/* Toolbar component to organize AppBar content */}
         <Toolbar disableGutters>
+
+            {/* Box component to center the page links */}
             <Box
             sx={{
                 flexGrow: 1,
@@ -22,6 +30,7 @@ return (
                 justifyContent: 'center',
             }}
             >
+                {/* Iterate through the pages array to create a Button and Link for each page */}
             {pages.map((page) => (
                 <Link
                 key={page}
