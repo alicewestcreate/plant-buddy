f4ad9a25c1bfd53566fc1c401cb93fc466044dbe
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-24 13:05:21 +0000

    import react, and additional mui imports

--- src/components/Sections/About.js
+++ src/components/Sections/About.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Fade from '@mui/material/Fade';
+import Slide from '@mui/material/Slide';
 
 
 
@@ -18,3 +22,4 @@ function About() {
               <Typography variant="h1" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
                 About Us
               </Typography>
+              </Fade>
