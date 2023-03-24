f682854fa17d80870d4526cc5fcca9846752f1e6
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-24 14:47:35 +0000

    Add browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, { Component } from 'react';
 import Home from './components/Home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import About from './components/Sections/About';
 import MyCards from './components/Card/Cards';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { purple } from '@mui/material/colors';
 import Test from "./sections/Test"
-import { Button } from "@mui/material";
-import TestPage from './components/TestPage';
 
 // overriding material ui's default themes 
 const theme = createTheme({
@@ -29,49 +29,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "React",
-      showHideHome: false
+      name: 'React',
+      showHideHome: false,
+      showHideAbout: false,
+  
     };
     this.hideComponent = this.hideComponent.bind(this);
   }
 
   hideComponent(name) {
     switch (name) {
-      case "showHideHome":
-        this.setState({ showHideHome: !this.state.showHideHome })
+      case 'showHideHome':
+        this.setState({ showHideHome: !this.state.showHideHome });
         break;
-      case "showHideTestPage":
-        this.setState({ showHideTestPage: !this.state.showHideTestPage });
+      case 'showHideAbout':
+        this.setState({ showHideAbout: !this.state.showHideAbout });
         break;
-      case "showHideButton":
-        this.setState({ showHideButton: !this.state.showHideButton })
-        break;
+      // ... (other cases)
       default:
         break;
     }
   }
 
   render() {
-    const { showHideHome, showHideTestPage, showHideButton } = this.state;
     return (
-      // wrap everything in theme provider 
       <ThemeProvider theme={theme}>
-        {/* navbar, home and button all show */}
-        {!showHideHome && <Navbar />}
-        {!showHideHome && <Home />}
-        {!showHideButton && (
-          // navbar, home page and button are hidden when button is clicked
-          <Button variant="contained" color="primary" onClick={() => this.hideComponent("showHideHome") & this.hideComponent("showHideTestPage") & this.hideComponent("showHideButton")} >
-            Click Me!
-          </Button>)}
-        {showHideTestPage && <TestPage />}
-        <Test/>
-        <MyCards/>
-        <Footer/>
-      </ThemeProvider >
-    )
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<><Home /><MyCards /></>} />
+            <Route path="/about" element={<About />} />
+            {/* Add other routes as needed */}
+          </Routes>
+          <Test />
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    );
   }
-
-}
+  
+}    
 
 export default App;
