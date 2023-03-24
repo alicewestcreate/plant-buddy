95e3083cf511b87c5f33c65b1c0b29b9bbb1d55d
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-24 13:28:21 +0000

    Add footer, code to align about us and contact us

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,31 +4,43 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 
-const pages = ['About Us', 'Contact Us'];
+const pages = ['About', 'Contact'];
 
 function ResponsiveAppBar() {
-    return (
-      <>
-        <Box sx={{ paddingBottom: '64px' }}></Box>
-        <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+  return (
+    <>
+      <Box sx={{ paddingBottom: '64px' }}></Box>
+      <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: 'none', md: 'flex' },
+                justifyContent: 'center',
+              }}
+            >
               {pages.map((page) => (
+                <Link
+                  key={page}
+                  to={`/${page.toLowerCase()}`}
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
                   <Button
-                    key={page}
                     sx={{ my: 2, color: 'white', display: 'block', mx: 1 }}
                   >
                     {page}
                   </Button>
-                ))}
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </>
-    );
-  }
-  export default ResponsiveAppBar;
-  
+                </Link>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
+  );
+}
+
+export default ResponsiveAppBar;
