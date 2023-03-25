8f5263d663fef2dc05253f97262249e5543b3afc
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-25 22:14:09 +0000

    Import Styled Carousel

--- src/components/Layout/Carousel/MyCarousel.js
+++ src/components/Layout/Carousel/MyCarousel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the CSS
-
+import StyledCarousel from './Carousel.styled';
 
 function MyCarousel() {
     return (
