47bcc229d4a4f6a3cf25c981f639c0c753221068
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-25 20:23:09 +0000

    Export Card, CardMedia, Typography and Button

--- src/components/Sections/Cards.styled.js
+++ src/components/Sections/Cards.styled.js
@@ -7,5 +7,23 @@ export const CardContainer = styled.div`
   min-height: 50vh;
 `;
 
+export const StyledCard = styled(Card)`
+    max-width: 345px;
+`;
+
+export const StyledCardMedia = styled(CardMEdia)`
+    height: 140px;
+    `;
+
+export const SunlightTypography = styled(Typography)`
+margin: 2;
+color: primary;
+variant: contained;
+gutterbottom: h5;
+`;
+
+export const StyledButtom = styled(Buttom)`
+    margin: 2;
+    `;
 
 
