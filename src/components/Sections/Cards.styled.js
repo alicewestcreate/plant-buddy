cb21786aa72178042207ebe5089e1b36bd5b0e62
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-25 20:14:05 +0000

    Import styled and export card container

---
+++ src/components/Sections/Cards.styled.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
+`;
+
+
+
