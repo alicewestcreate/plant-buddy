fa817ff7d7d1bcaf116dbf148b19865f4e267e90
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-25 21:26:31 +0000

    Add Carousel function with image

--- src/components/Layout/Carousel/MyCarousel.js
+++ src/components/Layout/Carousel/MyCarousel.js
@@ -2,5 +2,18 @@ import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the CSS
 
 
+function MyCarousel() {
+    return (
+        <StyledCarousel showArrows={false} showStatus={false} showThumbs={false} showIndicators={false} swipeable={false}>
+            <div>
+                <img src="../components/Images/kitchen-plant" alt="Kitchen Plant" />
+                <p className="legend">Kitchen Plant</p>
+            </div>
+        </StyledCarousel>
+    )
+}
+
+export default MyCarousel;
+
 
 
