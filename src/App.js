b725decebe2a53ee6b41fde87b00c5a131f528ec
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-26 19:53:57 +0100

    Add quiz and about route, import quiz, about, navbar, footer files to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import Navbar from './components/Layout/Navbar'
+import Footer from './components/Layout/Footer'
 import { Route, Routes } from "react-router-dom";
 import Home from './components/pages/Home';
-import Cards from './components/pages/Cards';
+import Quiz from './components/pages/Quiz';
+import About from './components/pages/About'
 import { ThemeProvider } from '@mui/material';
 import theme from "./theme";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cards" element={<Cards />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/about" element={<About />} />
       </Routes>
+      <Footer/>
     </ThemeProvider>
   )
 
