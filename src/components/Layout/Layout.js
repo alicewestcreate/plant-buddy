7c05add3f67aeba121dddf98df4a474360925779
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-26 18:06:21 +0100

    Change const function nd add import main content

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
 import { Main, MainContent } from './Layout.styled';
 
 const Layout = (props) => {
     const { children } = props;
     return (
         <Main>
-            <Navbar />
             <MainContent>
                 {children}
             </MainContent>
-            <Footer />
         </Main>
     )
 }
