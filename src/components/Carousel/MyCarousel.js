05f55b093f0c91004cfee5d4308286d96a56bdaa
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-27 11:49:56 +0100

    move file to new folder

--- src/components/Carousel/MyCarousel.js
+++ src/components/Carousel/MyCarousel.js
@@ -4,22 +4,10 @@ import { Paper, Box } from '@mui/material';
 
 
 
-// function MyCarousel() {
-//     return (
-//         <StyledCarousel showArrows={false} showStatus={false} showThumbs={false} showIndicators={false} swipeable={false}>
-//             <div>
-//                 <img src="../Images/kitchen-plant.jpg" alt="Kitchen Plant" />
-//                 <p className="legend">Kitchen Plant</p>
-//             </div>
-//         </StyledCarousel>
-//     )
-// }
-
-// export default MyCarousel;
-
-
 function CardWrapper() {
     return (
+        // The Paper component serves as a container for the card
+        // with a background image and some basic styling
         <Paper sx={{position: 'relative', minWidth: '50vh', minHeight: '100vh', mx: 'auto', background: 'url(./Images/kitchen-plant.jpg)'}}>
             <Box sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
                 <Cards />
