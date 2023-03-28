cd161b0543a3d4b86dbfbe1e5bda75d1736e7ba2
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-03-28 21:38:45 +0100

    scroll event added to navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -11,12 +11,32 @@ import ColorModeContext from '../../contexts/ColorModeContext';
 const Navbar = () => {
     // useNavigate built in hook from react
     const navigate = useNavigate();
-
+    // toggles between colors depending on dark or light mode
     const { toggleColorMode, mode } = useContext(ColorModeContext)
+
+    // navbar color when scroll down 
+    const [color, setColor] = useState(false);
+    const [textColor, setTextColor] = useState(false);
+    const changeColor = () => {
+        // if window is less than of equal to 100 pixels then set navbar background color
+        if (window.scrollY >= 100) {
+            setColor(true);
+            setTextColor(true)
+        } else {
+            setColor(false);
+            setTextColor(false)
+        }
+    }
+    window.addEventListener("scroll", changeColor);
 
     return (
         <Box>
-            <AppBar position="fixed" elevation={0} sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+            <AppBar position="fixed"
+                elevation={0} sx={{
+                    // if user scrolls give navbar a background color and boxshadow 
+                    backgroundColor: color ? 'primary.main' : 'transparent',
+                    boxShadow: color ? '0 2px 4px rgba(0, 0, 0, 0.3)' : 'none'
+                }}>
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, cursor: 'pointer' }} onClick={() => navigate("/")}>
                         Plant Buddy
